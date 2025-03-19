9fd3bb6ef27d2eeb31111f86149b7c5af5d03666
quoccuong <tranquoccuong0179@gmail.com>
2025-03-19 16:08:51 +0700

    update

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -15,7 +15,7 @@ import { JwtHelperService } from "@auth0/angular-jwt";
   providedIn: "root",
 })
 export class AuthService {
-  private baseUrl = "http://14.225.220.28:8182";
+  private baseUrl = "";
   private isAuthenticatedSubject = new BehaviorSubject<boolean>(false);
   isAuthenticated$ = this.isAuthenticatedSubject.asObservable();
 
